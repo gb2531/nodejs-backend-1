694429e8ac7755dd8401d79885e6ae66bb6f1447
gb2531 <balasurya1998@gmail.com>
2024-07-27 13:12:58 +0530

    updated login api

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,6 +6,7 @@ const bcrypt = require("bcrypt");
 const joi = require("joi");
 const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
+const e = require("express");
 
 //get all users
 router.get("/", (req, res, next) => {
@@ -112,22 +113,31 @@ router.patch("/:userId", async (req, res, next) => {});
 
 //login user
 router.post("/login", async (req, res, next) => {
-  console.log(req.body.userName);
-  const user = User.findOne({ userName: req.body.userName });
-  if (user) {
-    const token = jwt.sign(
-      { _id: this._id },
-      "a3488ad5c31f289d63f30be96e98f62d4abd356a57a7402f4d10ba2b45edcaf85c7a2f6e7ed3d0d9add1c8969f0631dbe696362d2890ea086c0ae96ec03f2bb3",
-      { expiresIn: "1d" }
-    );
-    return res.status(200).json({
-      userName: req.body.userName,
-      token: token,
+  await User.findOne({ userName: req.body.userName })
+    .exec()
+    .then(async (doc) => {
+      if (!doc) {
+        return res.status(404).json({ message: "No user found" });
+      } else {
+        const username = doc.userName;
+        let password = doc.password;
+        password = await bcrypt.compare(req.body.password, password);
+        if (!password) {
+          return res.status(400).json({ message: "wrong password" });
+        }
+        const token = jwt.sign(
+          { _id: this._id },
+          "a3488ad5c31f289d63f30be96e98f62d4abd356a57a7402f4d10ba2b45edcaf85c7a2f6e7ed3d0d9add1c8969f0631dbe696362d2890ea086c0ae96ec03f2bb3",
+          { expiresIn: "1d" }
+        );
+        return res.status(200).json({
+          userName: username,
+          token: token,
+        });
+      }
     });
-  } else {
-    res.status(401).json({ message: "Invalid credentials" });
-  }
 });
+
 //delete user
 router.delete("/:userId", (req, res, next) => {
   const id = req.params.userId;
